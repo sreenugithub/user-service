d70ed713f2d1dd4f6f4733f61631639ff0a7e1db
sreenugithub <csreenu.soft@gmail.com>
2021-07-13 00:40:19 +0530

    first commit

---
+++ src/main/java/com/fullstack/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.fullstack.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fullstack.user.entity.User;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
---
+++ src/main/java/com/fullstack/user/vo/ResponseTemplateVO.java
@@ -0,0 +1,16 @@
+package com.fullstack.user.vo;
+
+import com.fullstack.user.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
